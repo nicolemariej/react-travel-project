bb01de78a0b0556fa72c1fabfc2b8992463326d2
Nicole <nicolemariejones1@gmail.com>
2022-06-20 22:39:50 +0100

    Complete styling

--- my-app/src/components/cards.js
+++ my-app/src/components/cards.js
@@ -1,13 +1,16 @@
 import React from "react"
+import {BiBeenHere} from "react-icons/bi"
 
 export default function Cards(){
     return(
         <div className="cards">
             <img src="https://www.abercrombiekent.co.uk/-/media/abercrombieandkent/images/page-header-images/central-america/guatemala/lake-atitlan/lake-atitlan_0004_guatemala-pier-at-lake-atitlan-in-guatemala_shutterstock_189649244.jpg?w=1920&h=940" className="destination-img" alt="Destination Image"/>
             <div className="cards-content">
-                <h3>Guatemala</h3><span className="google-maps">View on Google Maps</span>
+                <span className= "country"><BiBeenHere />Guatemala</span>
+                <span className="google-maps"> View on Google Maps</span>
         <h2>Lake Atitlan</h2>
-
+<p className="dates">12 Jan, 2021 - 24 Jan, 2021</p>
+<p className="description">Lake Atitlan is a volcanic lake located in the middle of guatemala. There are numerous surrounding towns. As well as spanish, the locals speak their native language. The language they speak will depend on which part of the lake their town is located.</p>
             </div>
         </div>
     )
